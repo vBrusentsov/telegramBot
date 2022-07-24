2c1f7a2df5cb8b8af675b0b42b97f4b32386c3d6
Arconiel <Arconiel456@gmail.com>
2022-07-24 12:51:16 +0300

    style: lint index.js and README.md

--- index.js
+++ index.js
@@ -4,70 +4,69 @@ const { Bot } = require('grammy');
 dotenv.config();
 
 (() => {
-    const bot = new Bot(process.env.BOT_TOKEN);
+  const bot = new Bot(process.env.BOT_TOKEN);
 
-    bot.use((ctx, next) => {
-        ctx.state = {};
-        return next();
-    })
+  bot.use((ctx, next) => {
+    ctx.state = {};
+    return next();
+  });
 
-    const middlewareMatchToSplit = function (ctx, next) {
-        if (!ctx.match) {
-            return ctx.reply("Please enter a number");
-        }
-
-        ctx.state.arguments = ctx.match.split(' ');
-
-        const validateNumbers = ctx.state.arguments.some((elem) => {
-            return Number.isNaN(+elem);
-        });
-
-        if (validateNumbers) {
-            return ctx.reply("Incorrect number entered");
-        }
-
-        return next();
+  const middlewareMatchToSplit = function (ctx, next) {
+    if (!ctx.match) {
+      return ctx.reply('Please enter a number');
     }
 
-    bot.command("dimagay", (ctx)=> ctx.reply("WHY ARE YOU GAy"));
-    bot.command("start", (ctx) => ctx.reply("welcome! "));
-    bot.command("stop", (ctx) => ctx.reply("Okay. I`m will sleep"));
-    bot.command("add",middlewareMatchToSplit, (ctx) => {
-        const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
-            return +previousValue + +currentValue;
-        });
+    ctx.state.arguments = ctx.match.split(' ');
 
-        return ctx.reply(String(result));
+    const validateNumbers = ctx.state.arguments.some((elem) => {
+      return Number.isNaN(+elem);
     });
 
-    bot.command("subtract",middlewareMatchToSplit, (ctx) => {
-        const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
-            return +previousValue - +currentValue;
-        }, 0);
+    if (validateNumbers) {
+      return ctx.reply('Incorrect number entered');
+    }
 
-        return ctx.reply(String(result));
+    return next();
+  };
+
+  bot.command('dimagay', (ctx) => ctx.reply('WHY ARE YOU GAy'));
+  bot.command('start', (ctx) => ctx.reply('welcome! '));
+  bot.command('stop', (ctx) => ctx.reply('Okay. I`m will sleep'));
+  bot.command('add', middlewareMatchToSplit, (ctx) => {
+    const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
+      return +previousValue + +currentValue;
     });
 
+    return ctx.reply(String(result));
+  });
 
-    bot.command("multiply",middlewareMatchToSplit,  (ctx) => {
-        const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
-            return +previousValue * +currentValue;
-        });
+  bot.command('subtract', middlewareMatchToSplit, (ctx) => {
+    const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
+      return +previousValue - +currentValue;
+    }, 0);
 
-        return ctx.reply(String(result));
+    return ctx.reply(String(result));
+  });
+
+  bot.command('multiply', middlewareMatchToSplit, (ctx) => {
+    const result = ctx.state.arguments.reduce((previousValue, currentValue) => {
+      return +previousValue * +currentValue;
     });
 
-    bot.hears(/^\/.*/, (ctx) => {
-        ctx.reply("Please use one of the following commands: \n/add\n/subtract\n/multiply\n/start\n/stop ");
+    return ctx.reply(String(result));
+  });
+
+  bot.hears(/^\/.*/, (ctx) => {
+    ctx.reply(
+      'Please use one of the following commands: \n/add\n/subtract\n/multiply\n/start\n/stop ',
+    );
+  });
+
+  bot.hears('Hello', (ctx) => {
+    ctx.reply('Hello', {
+      reply_to_message_id: ctx.msg.message_id,
     });
+  });
 
-
-    bot.hears("Hello", (ctx) => {
-        ctx.reply("Hello", {
-            reply_to_message_id: ctx.msg.message_id,
-        });
-    });
-
-    bot.start();
+  bot.start();
 })();
-
